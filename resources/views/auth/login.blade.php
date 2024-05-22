5e085c543b5d5dba6c29deec706b4c1dbc015269
Higino Denilson Januário <Higino502011@hotmail.com>
2024-05-23 00:09:10 +0100

    Fixe

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,85 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-<body>
+  <head>
+    <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <title>Macon | Entrar</title>
+      
+      <!-- Favicon -->
+      <link rel="shortcut icon" href="images/favicon.ico" />
+      <link rel="stylesheet" href="css/backend-plugin.min.css">
+      <link rel="stylesheet" href="css/backend.css?v=1.0.2">
+      <link rel="stylesheet" href="vendor/@fortawesome/fontawesome-free/css/all.min.css">
+      <link rel="stylesheet" href="vendor/line-awesome/dist/line-awesome/css/line-awesome.min.css">
+      <link rel="stylesheet" href="vendor/remixicon/fonts/remixicon.css">
+      <link rel="stylesheet" href="vendor/@icon/dripicons/dripicons.css">  </head>
+  <body class=" ">
+    <!-- loader Start -->
+    <div id="loading">
+          <div id="loading-center">
+          </div>
+    </div>
+    <!-- loader END -->
     
-</body>
+      <div class="wrapper">
+      <section class="login-content">
+         <div class="container h-100">
+            <div class="row align-items-center justify-content-center h-100">
+               <div class="col-12">
+                  <div class="row align-items-center">
+                     <div class="col-lg-6">
+                        <h2 class="mb-2">Acesso ao Sistema</h2>
+                        <p></p>
+                        <form action="{{route('login')}}" method="POST">
+                           @csrf
+                           <div class="row">
+                              <div class="col-lg-12">
+                                 <div class="form-group">
+                                    <label class="mb-0">E-mail</label>
+                                    <input class="form-control" name="email" type="email" placeholder=" ">
+                                 </div>
+                              </div>
+                              <div class="col-lg-12">
+                                 <div class="form-group">
+                                    <label class="mb-0">Senha</label>
+                                    <input class="form-control" name="password" type="password" placeholder=" ">
+                                 </div>
+                              </div>
+                             
+                              
+                           </div>
+                           <button type="submit" class="btn btn-primary btn-lg">Entrar</button>
+                        </form>
+                     </div>
+                     <div class="col-lg-6 mb-lg-0 mb-4 mt-lg-0 mt-4">
+                        <img src="images/login/01.png" class="img-fluid w-80" alt="">
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      </div>
+    
+    <!-- Backend Bundle JavaScript -->
+    <script src="js/backend-bundle.min.js"></script>
+    
+    <!-- Table Treeview JavaScript -->
+    <script src="js/table-treeview.js"></script>
+    
+    <!-- SweetAlert JavaScript -->
+    <script src="js/sweetalert.js"></script>
+    
+    <!-- Vectoe Map JavaScript -->
+    <script src="js/vector-map-custom.js"></script>
+    
+    <!-- Chart Custom JavaScript -->
+    <script src="js/chart-custom.js"></script>
+    
+    <!-- slider JavaScript -->
+    <script src="js/slider.js"></script>
+    
+    <!-- app JavaScript -->
+    <script src="js/app.js"></script>
+  </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,12 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware'=>'auth'],function(){
+    Route::get('/', function () {
+        return view('pages.index');
+    });
 });
-Route::get('testar', function () {
-    return view('welcome');
-})->middleware('auth');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
