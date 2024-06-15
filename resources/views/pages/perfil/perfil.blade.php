4932280dad75287c4eb1fdf2f5f610e9f8145f32
Alfredocaconda <123181679+Alfredocaconda@users.noreply.github.com>
2024-06-15 06:37:43 -0300

    Perfil

--- resources/views/pages/perfil/perfil.blade.php
+++ resources/views/pages/perfil/perfil.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.base')
 
 @section('macon')
-    
+<div class="media-body">
+    <h4 class="mb-2">Meu Perfil</h4>
+        <p class="mb-0 font-size-14">Nome Completo : {{Auth::user()->name}}</p>
+        <p class="mb-0 font-size-14">E-mail : {{Auth::user()->email}}</p>
+        <p class="mb-0 font-size-14">Tipo : {{Auth::user()->tipo}}</p>
+</div>
 @endsection
